75075a8ba215ab99e4a638ded65021a0f8ca9fd1
weihan <weihan7@qq.com>
2026-03-09 16:59:20 +0800

    调整签到域名，调整Readme文档

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const glados = async () => {
   const cookie = process.env.GLADOS
   if (!cookie) return
+
+  //const domain = 'glados.rocks'
+  const domain = 'glados.cloud'
   try {
     const headers = {
       'cookie': cookie,
-      'referer': 'https://glados.rocks/console/checkin',
+      'referer': `https://${domain}/console/checkin`,
       'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
     }
-    const checkin = await fetch('https://glados.rocks/api/user/checkin', {
+    const checkin = await fetch(`https://${domain}/api/user/checkin`, {
       method: 'POST',
       headers: { ...headers, 'content-type': 'application/json' },
-      body: '{"token":"glados.one"}',
+      body: JSON.stringify({ token: domain }),
     }).then((r) => r.json())
-    const status = await fetch('https://glados.rocks/api/user/status', {
+    const status = await fetch(`https://${domain}/api/user/status`, {
       method: 'GET',
       headers,
     }).then((r) => r.json())
--- readme.md
+++ readme.md
@@ -10,11 +10,11 @@ GitHub Actions 实现 [GLaDOS][glados] 自动签到
 
 2. 登录 [GLaDOS][glados] 获取 Cookie
 
-3. 添加 Cookie 到 Secret `GLADOS`
+3. 打开 `项目Settings > Secrets and variables > Actions`，在 `Secrets` 中添加变量（New repository secret ），变量名：`GLADOS`，变量值：`登录 GLaDOS 的 Cookie`
 
 4. 启用 Actions, 每天北京时间 00:10 自动签到
 
-5. 如需推送通知, 可用 [PushPlus][pushplus], 添加 Token 到 Secret `NOTIFY`
+5. 如需推送通知, 可用 [PushPlus][pushplus], 与第 3 步一样添加变量，变量名：`NOTIFY`，变量值：`从 PushPlus 获取的密钥`
 
 [glados]: https://github.com/glados-network/GLaDOS
 [pushplus]: https://www.pushplus.plus/
